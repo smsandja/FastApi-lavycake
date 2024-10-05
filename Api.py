07fd98f3a9e4f79111159f1a063df4c52db6e530
Sam <smsandja@gmail.com>
2024-10-05 22:52:50 +0000

    Recherche par id

---
+++ Api.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+inventory = {
+        1: {
+            "name" : "Riz",
+            "price" : 2000,
+            "brand" : "regular"
+        },
+        2: {
+            "name" : "Pate",
+            "price" : 1500,
+            "brand" : "regular"
+        },
+        3: {
+            "name" : "Patato",
+            "price" : 1500,
+            "brand" : "regular"
+        }
+    }
+@app.get("/get-item/{item_id}")
+def get_item(item_id: int):
+    return inventory[item_id]
+
+@app.get("/")
+def home():
+    return {"Data" : "testing"}
+
+@app.get("/get-by-name")
+def get_item(name: str):
+    for item_id in inventory:
+        if inventory[item_id]["name"] == name:
+            return inventory[item_id]
+    return {"Data": "Not found"}
+
+
+    
+
+
+
+        
